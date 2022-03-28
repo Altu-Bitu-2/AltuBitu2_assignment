1157015015fd67e63434f7995edd46a76515c661
minkyung01 <min2000@ewhain.net>
2022-03-29 02:04:56 +0900

    0322 과제 수정

--- 0322/1009.cpp
+++ 0322/1009.cpp
@@ -3,27 +3,23 @@
 //
 
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int findLast(int aa, int bb) {
-    int last = 1; //마지막 데이터가 처리되는 컴퓨터 번호
+vector<int> findNum(int aa) {
+    vector<int> numA; //패턴을 저장할 배열 선언
 
-    for (int i = 0; i < bb; i++) {
-        //aa의 bb제곱 구하기 ex) aa = 2, bb = 3이라면 temp가 8이 되도록
-        last = (last * aa) % 10;
-    }
-    //1, 5, 6 등은 1개의 숫자가 계속 반복되는 걸 알긴 하는데
-    //배열에 특정 요소가 존재하는지 판단하는 방법을 모르겠어서...ㅠㅠ
-    //우선 이렇게 둡니다....!!
+    int first = aa; //첫 데이터가 처리되는 컴퓨터 번호
+    numA.push_back(first);
 
-    if (last == 0) {
-        //1의 자리가 0이라면 10번 컴퓨터가 처리함
-        last = 10;
+    int temp = first;
+    while (first != (temp * first) % 10) {
+        temp = (temp * first) % 10;
+        numA.push_back(temp);
     }
 
-    return last;
+    return numA;
 }
 
 int main() {
@@ -35,9 +31,11 @@ int main() {
     cin >> T;
 
     int a, b; //데이터의 개수는 항상 a^b개
-    for (int i = 0; i < T; i++) {
+    vector<int> arr;
+    while (T--) {
         cin >> a >> b;
-        cout << findLast(a, b) << "\n";
+        arr = findNum(a);
+        cout << arr[(b - 1) % arr.size()] << "\n";
     }
 
     return 0;
--- 0322/1316.cpp
+++ 0322/1316.cpp
@@ -8,33 +8,26 @@
 
 using namespace std;
 
-int groupCnt = 0; //그룹 단어 개수
-vector<bool> alphabet; //알파벳 등장 여부를 나타내는 배열 선언
-//a: 0번 인덱스 ~ z: 25번 인덱스
-
-void isGroupWord(string ss) {
-    bool isGroup = true; //그룹단어인지를 판단
+bool isGroupWord(string ss) { //그룹단어인지를 판단
+    vector<bool> alphabet; //알파벳 등장 여부를 나타내는 배열 선언
+    //a: 0번 인덱스 ~ z: 25번 인덱스
 
     alphabet.assign(26, false); //false로 초기화
-    alphabet[(int) (ss[0]) - 97] = true; //첫번째 문자 true로 바꾸기
+    alphabet[ss[0] - 'a'] = true; //첫번째 문자 true로 바꾸기
 
     for (int i = 1; i < ss.length(); i++) {
         if (ss[i] == ss[i - 1]) {
             //이전에 등장한 문자와 동일하면
             continue;
-        } else if (ss[i] != ss[i - 1] && !alphabet[(int) (ss[i]) - 97]) {
-            //처음 등장하는 문자라면
-            alphabet[(int) (ss[i]) - 97] = true;
-            continue;
-        } else if(ss[i] != ss[i-1] && alphabet[(int) (ss[i]) - 97]) {
+        } else if(alphabet[ss[i] - 'a']) {
             //기존에 등장했던 문자가 재등장했다면
-            isGroup = false;
+            return false;
+        } else {
+            //처음 등장하는 문자라면
+            alphabet[ss[i] - 'a'] = true;
         }
     }
-    if(isGroup) {
-        //그룹단어라면
-        groupCnt++;
-    }
+    return true;
 }
 
 int main() {
@@ -45,10 +38,14 @@ int main() {
     int N; //단어의 개수
     cin >> N;
 
-    string str;
-    for (int i = 0; i < N; i++) {
+    int groupCnt = 0;
+    while(N--) {
+        string str;
         cin >> str;
-        isGroupWord(str);
+
+        if(isGroupWord(str)) {
+            groupCnt++;
+        }
     }
 
     cout << groupCnt;
