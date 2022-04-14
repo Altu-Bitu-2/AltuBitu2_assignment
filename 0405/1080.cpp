eb0bb8a7acd297c07f44e8fae2321f8129b47b63
minkyung01 <min2000@ewhain.net>
2022-04-14 14:31:26 +0900

    0405 과제 수정

--- 0405/1080.cpp
+++ 0405/1080.cpp
@@ -3,26 +3,26 @@
 //
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int A[50][50]; //행렬 A
-int B[50][50]; //행렬 B
-
-void changeMatrix(int ii, int jj) {
-    for (int i = ii - 1; i <= ii + 1; i++) {
-        for (int j = jj - 1; j <= jj + 1; j++) {
-            A[i][j] = 1 - A[i][j];
+void changeMatrix(int row, int col, vector<string> &mat_a) {
+    for (int i = row; i < row + 3; i++) {
+        for (int j = col; j < col + 3; j++) {
+            if (mat_a[i][j] == '1') {
+                mat_a[i][j] = '0';
+            } else {
+                mat_a[i][j] = '1';
+            }
         }
     }
 }
 
-bool isPossible(int n, int m) {
+bool isPossible(int n, int m, vector<string> &mat_a, vector<string> &mat_b) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (A[i][j] != B[i][j]) {
+            if (mat_a[i][j] != mat_b[i][j]) {
                 return false;
             }
         }
@@ -38,37 +38,31 @@ int main() {
     int N, M; //N: 행렬의 row 개수, M: 행렬의 column 개수
     cin >> N >> M;
 
+    vector<string> a(N); //0과 1을 그 자체로 입력 받기 위해 string으로 설정
+    vector<string> b(N);
+
     for (int i = 0; i < N; i++) { //행렬 A 채우기
-        for (int j = 0; j < M; j++) {
-            cin >> A[i][j];
-        }
+        cin >> a[i]; //cin은 공백을 기준으로 입력 받음
     }
 
     for (int i = 0; i < N; i++) { //행렬 B 채우기
-        for (int j = 0; j < M; j++) {
-            cin >> B[i][j];
-        }
+        cin >> b[i];
     }
 
     int change = 0; //최소 변환 횟수 저장
     for (int i = 0; i < N - 2; i++) {
         for (int j = 0; j < M - 2; j++) {
-            if (A[i][j] != B[i][j]) { //변환이 필요한 경우
+            if (a[i][j] != b[i][j]) { //변환이 필요한 경우
+                changeMatrix(i, j, a);
                 change++;
-                changeMatrix(i + 1, j + 1);
             }
         }
     }
 
-    bool possible = isPossible(N, M);
-    if (!possible) {
-        cout << -1;
-    } else {
-        cout << change;
+    if (!isPossible(N, M, a, b)) {
+        change = -1;
     }
+    cout << change;
 
     return 0;
 }
-
-// 계속 코드가 실행이 안 되는데...
-// 혹시 코드에서 오류 찾아주실 수 있을까요?
--- 0405/11000.cpp
+++ 0405/11000.cpp
@@ -9,18 +9,23 @@
 
 using namespace std;
 
-vector<pair<int, int>> ts; //수업 시작 시간 & 종료 시간을 저장하는 pair
-priority_queue<int, vector<int>, greater<>> pq; //최소 힙 pq 선언
+typedef pair<int, int> st; //시작 시간 & 종료 시간
 
-int findMin(int n) { //n: 수업 개수
-    pq.push(ts[0].first); //가장 빨리 끝나는 수업의 종료 시간을 pq에 삽입
+int findMin(int n, vector<st> lec) { //n: 수업 개수
+    priority_queue<int, vector<int>, greater<>> pq; //최소 힙 pq 선언
 
-    for (int i = 1; i < n; i++) {
-        if (ts[i].second < pq.top()) {
-            //i번째 수업의 시작 시간이 현재 pq의 top보다 빠르다면
-            //같은 강의실 사용 불가 >> 강의실 추가하기
-            pq.push(ts[i].first);
+    pq.push(-1);
+
+    for(int i= 0; i<n; i++) {
+        if (lec[i].first >= pq.top()) {
+            //현재 종료 시간이 가장 이른 수업보다
+            //lec[i]의 시작시간이 같거나 더 느리다면
+            pq.pop(); //강의실 함께 사용 가능하므로
         }
+
+        //(if 조건에 만족하지 않을 경우) 새로운 강의실을 하나 추가하거나
+        //(if 조건을 만족하는 경우) 기존 강의실 종료 시간 갱신신
+       pq.push(lec[i].second);
     }
 
     return pq.size();
@@ -34,19 +39,14 @@ int main() {
     int N; //N: 수업 개수
     cin >> N;
 
+    vector<st> lecture(N, st(0, 0));
+
     for (int i = 0; i < N; i++) {
-        int s, t; //s: 시작 시간, t: 종료 시간
-        cin >> s >> t;
-
-        ts.push_back(make_pair(t, s));
+        cin >> lecture[i].first >> lecture[i].second;
     }
-    sort(ts.begin(), ts.end()); //종료 시간이 빠른 순으로 정렬
+    sort(lecture.begin(), lecture.end()); //종료 시간이 빠른 순으로 정렬
 
-    cout << findMin(N);
+    cout << findMin(N, lecture) << "\n";
 
     return 0;
 }
-
-//이것도 코드 봐주실 수 있을까요ㅠㅠ
-//주어진 예제 입력이랑 제가 임의로 몇개 더 넣어봤는데도 잘 나오는데
-//백준에서는 틀렸다고 나오네요ㅠㅠㅠ
